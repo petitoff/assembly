a8d75a9a4adb951abb47b73e3092ef6ce64aedee
Błażej Domagała <petitoff@outlook.com>
2022-03-21 17:48:11 +0000

    Create startup project

---
+++ VIsual Studio work setup/StartSetup/StartSetup.cpp
@@ -0,0 +1,13 @@
+// StartSetup.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "stdafx.h"
+
+int main()
+{
+	__asm
+	{
+
+	}
+	std::cin.get();
+}
